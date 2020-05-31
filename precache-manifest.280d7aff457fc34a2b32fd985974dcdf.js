a81f1aa697d6b13ff84adbea98523c3d4c98a1d8
Jayanttokas <55356411+Jayanttokas@users.noreply.github.com>
2020-05-31 15:25:53 +0530

    Updates

---
+++ precache-manifest.280d7aff457fc34a2b32fd985974dcdf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "738755ca97646feaf2cbadf25973a05b",
+    "url": "/CoronaReactApp/index.html"
+  },
+  {
+    "revision": "d12c8ca9d4284a4ba4c5",
+    "url": "/CoronaReactApp/static/js/2.c3eb278a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CoronaReactApp/static/js/2.c3eb278a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5db95c6db7c43b2ba93",
+    "url": "/CoronaReactApp/static/js/main.64388236.chunk.js"
+  },
+  {
+    "revision": "4a60d3642921963bc9a1",
+    "url": "/CoronaReactApp/static/js/runtime-main.aac5a071.js"
+  }
+]);
